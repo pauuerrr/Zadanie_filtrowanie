52961ae245ac19db10b6bda9de36430829adad33
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-23 15:25:30 +0200

    Dodaj pliki projektów.

---
+++ Zadanie_filtrowanie/Utils/CustomFilterAbilities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zadanie_filtrowanie.Utils
+{
+    public class CustomFilterAbilities
+    {
+    }
+}
