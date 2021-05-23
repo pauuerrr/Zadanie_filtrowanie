accb5c5bceefacf8f2ec17e13194dcb7a09b05f1
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-23 15:38:47 +0200

    Dodanie metody OnResultExecutionAsync

--- Zadanie_filtrowanie/Utils/CustomFilterAbilities.cs
+++ Zadanie_filtrowanie/Utils/CustomFilterAbilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace Zadanie_filtrowanie.Utils
 {
     public class CustomFilterAbilities
     {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        { 
+        
+        }
     }
 }
